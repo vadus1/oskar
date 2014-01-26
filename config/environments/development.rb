6c5355ce23de8d7f63d650e4ed4ebae308cfe06d
Vadus <bekish94.94@mail.ru>
2014-01-27 01:27:41 +0300

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,11 +32,11 @@ Oskar::Application.configure do
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
+    :address              => "smtp.mail.ru",
+    :port                 => 465,
     :domain               => "localhost:3000",
-    :user_name            => "vadus.by@gmail.com",
-    :password             => "UhkQGh2c",
+    :user_name            => "bekish94.94@mail.ru",
+    :password             => "vadus",
     :authentication       => :plain,
     :enable_starttls_auto => true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,12 @@ Oskar::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => "95.85.58.135" }
   config.action_mailer.smtp_settings = {
-        :address => "smtp.gmail.com",
-        :port => 587,
-        :domain => '95.85.58.135:80',
-        :user_name => "vadus.by@gmail.com",
-        :password => "UhkQGh2c",
-        :authentication => :plain,
-        :enable_starttls_auto => true
+    :address              => "smtp.mail.ru",
+    :port                 => 465,
+    :domain               => '95.85.58.135:80',
+    :user_name            => "bekish94.94@mail.ru",
+    :password             => "vadus",
+    :authentication       => :plain,
+    :enable_starttls_auto => true
   }
 end
