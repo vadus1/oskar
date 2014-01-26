e6f562255189350de4fcba1616e562f7af77d75f
Vadus <bekish94.94@mail.ru>
2014-01-27 00:30:25 +0300

    fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Не корректный emal"
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Не корректный email"
   validates :name, :email, :body, presence: true
 end
