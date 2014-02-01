df78b0283edca61d05cda248f6a265f4843509e9
Vadus <bekish94.94@mail.ru>
2014-02-01 03:16:34 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module Oskar
   class Application < Rails::Application
-    config.assets.precompile += %w(application.css theme.css main.js)
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
